dedfbf5a30a8bdb9e94b4f85d56c36d0207b016f
Russell Tan <50812868+sine-over-cosine@users.noreply.github.com>
2020-02-01 17:05:41 +0800

    Update Special_number Identifier.py

--- Special_number Identifier.py
+++ Special_number Identifier.py
@@ -1,10 +1,14 @@
-def is_special(p1):
-  if(len(p1)%2==1):
-    return False
+def is_special(num_str,types):
+  if types == "A5" or types == "A6":
+      length_of_p1=len(num_str)-1
   else:
-    m = len(p1)//2
-    print(m)
-    if p1[-m]==0 or p1[-1-m]==0:
-      return True
-    else:
+      length_of_p1=len(num_str)
+  if length_of_p1%2!=0:
       return False
+  else:
+      num=num_str[::-1]
+      m=length_of_p1>>1
+      if int(num[m])==0 or int(num[m-1])==0:
+          return True
+      else:
+          return False
