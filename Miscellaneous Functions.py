72226b629c7e0c59328fc57e64ee6302583bff1d
Russell Tan <50812868+sine-over-cosine@users.noreply.github.com>
2020-02-01 17:11:18 +0800

    Update Miscellaneous Functions.py

--- Miscellaneous Functions.py
+++ Miscellaneous Functions.py
@@ -13,3 +13,17 @@ def parse_list_to_int(lst):
     for num in lst:
         string=string+str(num)
     return int(string)
+
+def display(p1,p2,p3,num_string):
+    r1,r2,r3='','',''
+    for i in range(0,len(p1)):
+        r1=r1+str(p1[i])
+    for j in range(0,len(p2)):
+        r2=r2+str(p2[j])
+    for k in range(0,len(p3)):
+        r3=r3+str(p3[k])
+    print(r1)
+    print("+"+r2)
+    print("+ "+r3)
+    print("----------------------")
+    print(num_string)
