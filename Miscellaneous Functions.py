8c227ee99b2993d16f5f3f314b9525f28ee14d8f
Russell Tan <50812868+sine-over-cosine@users.noreply.github.com>
2020-02-01 17:07:27 +0800

    Create Miscellaneous Functions.py

---
+++ Miscellaneous Functions.py
@@ -0,0 +1,15 @@
+def d(num,base):
+    return num%base
+  
+def str_to_lst(num):
+  num=str(num)
+  lst=[]
+  for digit in num:
+    lst.append(int(digit))
+  return lst  
+
+def parse_list_to_int(lst):
+    string=''
+    for num in lst:
+        string=string+str(num)
+    return int(string)
