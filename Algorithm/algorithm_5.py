d91b71c7dfc4522d738cbedfba4c221ca3b014af
Russell Tan <50812868+sine-over-cosine@users.noreply.github.com>
2020-02-02 21:14:18 +0800

    Update algorithm_5.py

--- Algorithm/algorithm_5.py
+++ Algorithm/algorithm_5.py
@@ -3,7 +3,6 @@ def algorithm_5(p1,p2,p3,base,num_string):
     s=base**l + base**(l-1)
     number=int(num_string)
     n_prime=number-s
-    
     lst=str_to_lst(n_prime)
     if lst[-l]==0 or lst[-1-l]==0:
         n_prime=n_prime-s
@@ -14,9 +13,11 @@ def algorithm_5(p1,p2,p3,base,num_string):
         k=1
     if types=="A1" or types=="A2" or types=="A3" or types=="A4":
         p1,p2,p3=algorithm_2(p1,p2,p3,base,str(n_prime),2)
-        print("algorithm_2")
     elif types=="B1" or types=="B2" or types=="B3" or types=="B4" or types=="B5" or types=="B6" or types=="B7":
         p1,p2,p3=algorithm_4(p1,p2,p3,base,str(n_prime),2)
     p1=parse_list_to_int(p1)+k*s
     p1=str_to_lst(str(p1))
-    return display(p1,p2,p3,num_string)
+    if base==16:
+        return hex_display(p1,p2,p3,num_string)
+    else:
+        return display(p1,p2,p3,num_string)
