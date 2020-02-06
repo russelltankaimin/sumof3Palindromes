68d042db200dbe11467ff9eb3f8f1e38b02569bf
Russell Tan <50812868+sine-over-cosine@users.noreply.github.com>
2020-02-06 22:25:26 +0800

    Create 3_digits sample output.py

---
+++ Algorithm/3_digits sample output.py
@@ -0,0 +1,83 @@
+Enter its base
+5
+
+Enter your number
+412
+Digit Length less than or equal to 7. Special Algorithm Casting...
+404
++3
++ 0
+----------------------
+[4, 1, 2]
+
+Enter its base
+6
+
+Enter your number
+524
+Digit Length less than or equal to 7. Special Algorithm Casting...
+515
++5
++ 0
+----------------------
+[5, 2, 4]
+
+Enter its base
+7
+
+Enter your number
+566
+Digit Length less than or equal to 7. Special Algorithm Casting...
+565
++1
++ 0
+----------------------
+[5, 6, 6]
+
+Enter its base
+8
+
+Enter your number
+734
+Digit Length less than or equal to 7. Special Algorithm Casting...
+727
++5
++ 0
+----------------------
+[7, 3, 4]
+
+Enter its base
+9
+
+Enter your number
+981
+Digit Length less than or equal to 7. Special Algorithm Casting...
+979
++1
++ 0
+----------------------
+[9, 8, 1]
+
+Enter its base
+10
+
+Enter your number
+341
+Digit Length less than or equal to 7. Special Algorithm Casting...
+333
++8
++ 0
+----------------------
+[3, 4, 1]
+
+Enter its base
+12
+
+Enter the base-12 number ,format DEADBEEF without the 0x prefix
+B37
+Digit Length less than or equal to 7. Special Algorithm Casting...
+B2B
++8
++ 0
+--------------------------
+B37
