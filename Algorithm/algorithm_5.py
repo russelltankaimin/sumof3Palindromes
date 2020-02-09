825e7130b1e7a7b474f2b872405a5c0e15d481ba
Russell Tan <50812868+sine-over-cosine@users.noreply.github.com>
2020-02-09 22:19:52 +0800

    Finally understood what the frick Algorithm V case 2 meant

--- Algorithm/algorithm_5.py
+++ Algorithm/algorithm_5.py
@@ -1,6 +1,6 @@
-def algorithm_5(p1,p2,p3,base,num_string):
+def algorithm_5(p1,p2,p3,base,num_string,val):
     print("Algorithm V")
-    l=len(num_string)>>1
+    l=len(num_string)//2
     s=base**l + base**(l-1)# s is an integer
     number=num_string
     #t_num=toDeci(number,base)
@@ -9,8 +9,9 @@ def algorithm_5(p1,p2,p3,base,num_string):
     n_prime=str(n_prime)#str
     lst=str(n_prime)[::-1]#this line should turn hex_int_to_hex_string
     if lst[l]=='0' or lst[l-1]=='0':
+        n_prime=hex_int_to_lst(n_prime)
         n_prime=base_add_sub(n_prime,t_s,base,0) # output would have been "ADE000C5" form
-        rekt=hex_int_to_lst(n_prime)
+        rekt=hex_int_to_lst(str(n_prime))
         types=check_type(rekt,base)
         k=2
     else:
@@ -52,20 +53,26 @@ def algorithm_5(p1,p2,p3,base,num_string):
         length_of_p1=actual_length
     if length_of_p1 %2==0:
         if types=="A1" or types=="A2" or types=="A3" or types=="A4":
-            if even:
-                p1,p2,p3=algorithm_2(p1,p2,p3,base,rekt,2)
-            else:
-                p1,p2,p3=algorithm_1(p1,p2,p3,base,rekt,2)
-        elif types=="A5" or types=="A6":
             if not even:
+                p1,p2,p3=algorithm_1(p1,p2,p3,base,rekt,2)
+            elif is_special(rekt,types)==False:
                 p1,p2,p3=algorithm_2(p1,p2,p3,base,rekt,2)
             else:
-                p1,p2,p3=algorithm_1(p1,p2,p3,base,rekt,2)
-        elif types=="B1" or types=="B2" or types=="B3" or types=="B4" or types=="B5" or types=="B6" or types=="B7":
+                p1,p2,p3=algorithm_5(p1,p2,p3,base,rekt,2)
+        elif types=="A5" or types=="A6":
             if even:
+                p1,p2,p3=algorithm_1(p1,p2,p3,base,rekt,2)
+            elif is_special(rekt,types)==False:
+                p1,p2,p3=algorithm_2(p1,p2,p3,base,rekt,2)
+            else:
+                p1,p2,p3=algorithm_5(p1,p2,p3,base,rekt,2)
+        elif types=="B1" or types=="B2" or types=="B3" or types=="B4" or types=="B5" or types=="B6" or types=="B7":
+            if not even:
+                p1,p2,p3=algorithm_3(p1,p2,p3,base,rekt,2)
+            elif is_special(rekt,types)==False:
                 p1,p2,p3=algorithm_4(p1,p2,p3,base,rekt,2)
             else:
-                p1,p2,p3=algorithm_3(p1,p2,p3,base,rekt,2)
+                p1,p2,p3=algorithm_5(p1,p2,p3,base,rekt,2)
     else:
         z1=d(int(rekt[-1])-int(rekt[2]),base)
         if int(rekt[0])==1 and int(rekt[1])<=2 and z1!=0:
@@ -81,10 +88,13 @@ def algorithm_5(p1,p2,p3,base,num_string):
         p1,p2,p3=algorithm_4(p1,p2,p3,base,rekt,2)
     x_add=fromDeci(k*s,base)
     x_add=lst_to_hex_int(x_add)
-   # p1=lst_to_hex_int(p1) [P1 is currently [10,0,0,0,0,0,0,10]. Goal==> Turn it to ...Nevermind]
+   # p1=lst_to_hex_int(p1) [P1 is currently [10,0,0,0,0,0,0,10]. Goal==> Turn it to 
     p1=base_add_sub(p1,x_add,base,1)
     p1=hex_int_to_lst(p1)
-    if base>10:
-        return hex_display(p1,p2,p3,num_string)
+    if val==2:
+        return p1,p2,p3
     else:
-        return display(p1,p2,p3,num_string)
+        if base>10:
+            return hex_display(p1,p2,p3,num_string)
+        else:
+            return display(p1,p2,p3,num_string)
