461be1a3cb5150827d6f77cf2c302c56e4994150
Russell Tan <50812868+sine-over-cosine@users.noreply.github.com>
2020-02-18 11:15:01 +0800

    Update algorithm_2.py

--- Algorithm/algorithm_2.py
+++ Algorithm/algorithm_2.py
@@ -1,67 +1,59 @@
-def algorithm_2(p1,p2,p3,base,num_string,val=1):
-    num=num_string[::-1]
+def algorithm_2(number,config,base,types):
+    debug("Algorithm_2")
+    if types=="A5" or types=="A6":
+        number=number[1:]
+    else:
+        pass
+    num=number[::-1]
     length=len(num)
-    if length%2==0:
-        m=length//2
-    else:
-        m=(length-1)//2
-    carry=[]
-    carry.append((p1[0]+p2[0]+p3[0])//base)
-    if p3[0]<=int(num[2*m-3])-1:
-        p1.append(d(int(num[2*m-2])-p2[0],base))
-    elif p3[0]>=int(num[2*m-3]):
-        p1.append(d(int(num[2*m-2])-p2[0]-1,base))
-    p2.append(d(int(num[2*m-3])-p3[0]-1,base))
-    p3.append(d(int(num[1])-p1[1]-p2[1]-carry[0],base))
-    carry.append((p1[1]+p2[1]+p3[1]+carry[0]-int(num[1]))//base)
+    m=length>>1
+    x,y,z=config[0],config[1],config[2]
+    x=[0]+x
+    y=[0]+y
+    z=[0]+z
+    c=[0,(x[1]+y[1]+z[1])//base]
+    x.append(d(num[2*m-2]-y[1],base) if z[1]<=num[2*m-3]-1 else d(num[2*m-2]-y[1]-1,base))
+    y.append(d(num[2*m-3]-z[1]-1,base))
+    z.append(d(num[1]-x[2]-y[2]-c[1],base))
+    c.append((x[2]+y[2]+z[2]+c[1]-num[1])//base)
     for i in range(3,m):
-        if p3[i-2]<=int(num[2*m-i-1])-1:
-            p1.append(1)
-        elif p3[i-2]>=int(num[2*m-i-1]):
-            p1.append(0)
-        p2.append(d(int(num[2*m-i-1])-p3[i-2]-1,base))
-        p3.append(d(int(num[i-1])-p1[i-1]-p2[i-1]-carry[i-2],base))
-        carry.append((p1[i-1]+p2[i-1]+p3[i-1]+carry[i-2]-int(num[i-1]))//base)
-    p1.append(0)
-    p2.append(d(int(num[m-1])-p3[m-2]-carry[m-2],base))
-    carry.append((p1[m-1]+p2[m-1]+p3[m-2]-carry[m-2])//base)
-    vital_carry=carry[m-1]
-    if vital_carry==1:
-        print("No Adjustment Needed")
-    elif vital_carry==0:
-        if p2[m-1]!=0:
-            p1[m-1]=1
-            p2[m-1]=p2[m-1]-1
+        x.append(1 if z[i-1]<=num[2*m-i-1]-1 else 0)
+        y.append(d(num[2*m-i-1]-z[i-1]-1,base))
+        z.append(d(num[i-1]-x[i]-y[i]-c[i-1],base))
+        c.append((x[i]+y[i]+z[i]+c[i-1]-num[i-1])//base)
+    x.append(0)
+    y.append(d(num[m-1]-z[m-1]-c[m-1],base))
+    c.append((x[m]+y[m]+z[m-1]+c[m-1]-num[m-1])//base)
+    if c[m]==1:
+        print("do nothing")
+    elif c[m]==0:
+        if y[m]!=0:
+            x[m]=1
+            y[m]-=1
         else:
-            if p2[m-2]!=0:
-                p1[m-1]=1
-                p2[m-1]=base-2
-                p2[m-2]=p2[m-2]-1
-                p3[m-2]=p3[m-2]+1
-            elif p2[m-2]== 0 and p3[m-2]!=0:
-                p2[m-2]=1
-                p2[m-1]=1
-                p3[m-2]=p3[m-2]-1
-            elif p2[m-2]== 0 and p3[m-2]==0:
-                p3[m-2]=2
-                p2[m-2]=base-1
-                p2[m-1]=base-4
-                p1[m-1]=1
-                p1[m-2]=p1[m-2]-1
-    elif vital_carry==2:
-        p1[m-1]=1
-        p2[m-1]=base-2
-        p2[m-2]=p2[m-2]-1
-        p3[m-2]=0
-    p1=p1+p1[::-1]
-    temp_p2=p2[::-1]
-    temp_p2=temp_p2[1:m+1]
-    p2=p2+temp_p2
-    p3=p3+p3[::-1]
-    if val==2:
-        return p1,p2,p3
-    else:
-        if base==16:
-            return hex_display(p1,p2,p3,num_string)
-        else:
-            return display(p1,p2,p3,num_string)
+            if y[m-1]!=0:
+                x[m]=1
+                y[m]=base-2
+                y[m-1]-=1
+                z[m-1]+=1
+            elif y[m-1]==0 and z[m-1]!=0:
+                y[m]=1
+                y[m-1]=1
+                z[m-1]-=1
+            elif y[m-1]==0 and z[m-1]==0:
+                x[m-1]-=1
+                x[m]=1
+                y[m]=base-4
+                y[m-1]=base-1
+                z[m-1]=2
+    elif c[m]==2:
+        x[m]=1
+        y[m-1]-=1
+        y[m]=base-2
+        z[m-1]=0
+    x,y,z=x[1:],y[1:],z[1:]
+    x,z=x+x[::-1],z+z[::-1]
+    t_y=y[::-1]
+    y=y+t_y[1:]
+    config=[x,y,z]
+    return config
